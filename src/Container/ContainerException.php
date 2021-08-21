fd612b2fd992220ac8faef4e18febfbfb1510952
Christopher Neven <cph.neven@gmail.com>
2021-08-21 14:27:03 +0200

    PHPCS

--- src/Container/ContainerException.php
+++ src/Container/ContainerException.php
@@ -1,12 +1,26 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Cphne\PsrTests\Container;
 
+use JetBrains\PhpStorm\Pure;
 use Throwable;
 
+/**
+ * Class ContainerException
+ * @package Cphne\PsrTests\Container
+ */
 class ContainerException extends \Exception implements \Psr\Container\ContainerExceptionInterface
 {
-    public function __construct($message = '', $code = 500, Throwable $previous = null)
+    /**
+     * Construct the exception. Note: The message is NOT binary safe.
+     * @link https://php.net/manual/en/exception.construct.php
+     * @param string $message [optional] The Exception message to throw.
+     * @param int $code [optional] The Exception code.
+     * @param Throwable|null $previous [optional] The previous throwable used for the exception chaining.
+     */
+    #[Pure] public function __construct($message = '', $code = 500, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
--- src/Services/Finder/FinderInterface.php
+++ src/Services/Finder/FinderInterface.php
@@ -1,12 +1,20 @@
 <?php
 
+declare(strict_types=1);
 
 namespace Cphne\PsrTests\Services\Finder;
 
-
+/**
+ * Interface FinderInterface
+ * @package Cphne\PsrTests\Services\Finder
+ */
 interface FinderInterface
 {
 
+    /**
+     * @param $subject
+     * @return mixed
+     */
     public function find($subject): mixed;
 
 }
